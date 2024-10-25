4dd851dd4f989db2e00b5d209b62c4c0d85300d1
Tomas Molina <molinatomas011@gmail.com>
2024-10-25 16:34:47 -0300

    Base Spring

---
+++ src/main/java/com/uade/gymapp/Application.java
@@ -0,0 +1,11 @@
+package src.main.java.com.uade.gymapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/uade/gymapp/Controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.uade.gymapp.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "¡Hola, mundo!";
+    }
+}
