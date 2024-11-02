670bf6f9089f961c9a3e8c8f5aeac2bc1cd4128e
Juaniliberto <juaniliberto@gmail.com>
2024-11-02 12:36:46 -0300

    Estados entrenamientos agregados

---
+++ src/main/java/com/uade/gymapp/model/EntrenamientoCompletado.java
@@ -0,0 +1,15 @@
+package com.uade.gymapp.model;
+
+import com.uade.gymapp.model.Intefaces.EstadoEntrenamiento;
+
+public class EntrenamientoCompletado implements EstadoEntrenamiento {
+    @Override
+    public void comenzar() {
+
+    }
+
+    @Override
+    public void finalizar() {
+
+    }
+}
