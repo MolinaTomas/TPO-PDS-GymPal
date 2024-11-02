26ed24f6c6bc4ee39835f1124a935d07e4504694
Tomas Molina <molinatomas011@gmail.com>
2024-11-02 20:36:38 -0300

    Trofeo Creido
    Hecho por Rami

--- src/main/java/com/uade/gymapp/model/TrofeoCreido.java
+++ src/main/java/com/uade/gymapp/model/TrofeoCreido.java
@@ -1,6 +1,7 @@
 package com.uade.gymapp.model;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.time.LocalDate;
 
 public class TrofeoCreido extends Trofeo{
@@ -12,13 +13,21 @@ public class TrofeoCreido extends Trofeo{
     @Override
     public boolean cumpleCondicion(Socio socio) {
         List<Medicion> mediciones = socio.getMediciones();
-        List<Medicion> medicionesDelMes;
+        List<Medicion> medicionesDelMes = new ArrayList<>();
         LocalDate fechaActual = LocalDate.now();
         int mesActual = fechaActual.getMonthValue();
         int anioActual = fechaActual.getYear();
 
         for (Medicion medicion : mediciones) {
-            if (medicion.getFecha().getMonth()==mesActual-1 && medicion.)
+            if (medicion.getFecha().getMonth()==mesActual-1 && medicion.getFecha().getYear()==anioActual) {
+                medicionesDelMes.add(medicion);
+            }
+        }
+
+        if (medicionesDelMes.size()>=3) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
