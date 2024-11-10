7d955df2a7596216a2d195ad5a6c19e3c67203e9
GabrielAran <gabitoaran@gmail.com>
2024-11-10 17:09:20 -0300

    Bases para fillear entrenamientos, ejercicios harcodeados

--- gymapp/gymapp/src/main/java/com/uade/gymapp/gymapp/controller/RutinaController.java
+++ gymapp/gymapp/src/main/java/com/uade/gymapp/gymapp/controller/RutinaController.java
@@ -1,5 +1,17 @@
 package com.uade.gymapp.gymapp.controller;
 
-public class RutinaController {
+import com.uade.gymapp.gymapp.model.Ejercicio;
+import com.uade.gymapp.gymapp.model.Entrenamiento;
+import com.uade.gymapp.gymapp.model.Socio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RutinaController { //AHORA LLENO LOS DATOS
+    private List<Entrenamiento> entrenamientosTonificar = new ArrayList<>();
+    private List<Entrenamiento> entrenamientosMantener = new ArrayList<>();
+    private List<Entrenamiento> entrenamientosBajar = new ArrayList<>();
+    private List<Ejercicio> ejerciciosTotales = new ArrayList<>();
+
 
 }
