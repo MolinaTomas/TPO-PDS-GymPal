f205624e21c113366f4e90e24a6e27c07732c051
Tomas Molina <molinatomas011@gmail.com>
2024-11-11 01:47:13 -0300

    refuerzoRutina

--- gymapp/gymapp/src/main/java/com/uade/gymapp/gymapp/controller/RutinaController.java
+++ gymapp/gymapp/src/main/java/com/uade/gymapp/gymapp/controller/RutinaController.java
@@ -16,19 +16,24 @@ public class RutinaController {
     private List<Ejercicio> inicializarEjercicios() {
         List<Ejercicio> ejercicios = new ArrayList<>();
         // Ejercicios de cardio (alto nivel aeróbico)
-        ejercicios.add(new Ejercicio(1L, "Cinta de correr", "Carrera continua con intervalos de intensidad", 3, 1, 0.0, 8, ExigenciaMuscular.BAJO, "url_video_cinta"));
+        ejercicios.add(new Ejercicio(1L, "Cinta de correr", "Carrera continua con intervalos de intensidad", 3, 1, 0.0,
+                8, ExigenciaMuscular.BAJO, "url_video_cinta"));
 
         // Ejercicios de fuerza (pecho)
-        ejercicios.add(new Ejercicio(2L, "Press de banca", "Press de banca con barra", 4, 12, 40.0, 2, ExigenciaMuscular.ALTO, "url_video_press"));
+        ejercicios.add(new Ejercicio(2L, "Press de banca", "Press de banca con barra", 4, 12, 40.0, 2,
+                ExigenciaMuscular.ALTO, "url_video_press"));
 
         // Ejercicios de piernas
-        ejercicios.add(new Ejercicio(3L, "Sentadillas", "Sentadillas con peso corporal", 4, 15, 0.0, 4, ExigenciaMuscular.ALTO, "url_video_sentadillas"));
+        ejercicios.add(new Ejercicio(3L, "Sentadillas", "Sentadillas con peso corporal", 4, 15, 0.0, 4,
+                ExigenciaMuscular.ALTO, "url_video_sentadillas"));
 
         // Ejercicios de core
-        ejercicios.add(new Ejercicio(4L, "Plancha", "Plancha isométrica", 3, 1, 0.0, 3, ExigenciaMuscular.MEDIO, "url_video_plancha"));
+        ejercicios.add(new Ejercicio(4L, "Plancha", "Plancha isométrica", 3, 1, 0.0, 3, ExigenciaMuscular.MEDIO,
+                "url_video_plancha"));
 
         // Ejercicios de espalda
-        ejercicios.add(new Ejercicio(5L, "Dominadas", "Dominadas en barra", 4, 8, 0.0, 3, ExigenciaMuscular.ALTO, "url_video_dominadas"));
+        ejercicios.add(new Ejercicio(5L, "Dominadas", "Dominadas en barra", 4, 8, 0.0, 3, ExigenciaMuscular.ALTO,
+                "url_video_dominadas"));
 
         return ejercicios;
     }
@@ -37,8 +42,9 @@ public class RutinaController {
         List<Ejercicio> ejerciciosSeleccionados = seleccionarEjerciciosSegunObjetivo(objetivo);
 
         List<Entrenamiento> entrenamientos = new ArrayList<>();
-        for (int i=0;i<20;i++) {
-            Entrenamiento entrenamiento = new Entrenamiento(9223372036854775807L, 45, new Date(), ejerciciosSeleccionados);
+        for (int i = 0; i < 20; i++) {
+            Entrenamiento entrenamiento = new Entrenamiento(9223372036854775807L, 45, new Date(),
+                    ejerciciosSeleccionados);
             entrenamientos.add(entrenamiento);
         }
 
@@ -65,7 +71,8 @@ public class RutinaController {
                 }
             }
         } else {
-            // Para mantener figura: ejercicios con nivel aeróbico entre 2 y 4 y exigencia media-baja
+            // Para mantener figura: ejercicios con nivel aeróbico entre 2 y 4 y exigencia
+            // media-baja
             for (Ejercicio ejercicio : ejerciciosDisponibles) {
                 if (ejercicio.getNivelAerobico() >= 2 &&
                         ejercicio.getNivelAerobico() <= 4 &&
@@ -77,5 +84,21 @@ public class RutinaController {
         }
 
         return ejerciciosSeleccionados;
+    }
+
+    public void refuerzoRutina(Rutina rutina) {
+        if (rutina == null || rutina.getEntrenamientos() == null) {
+            System.out.println("Rutina o entrenamientos no disponibles");
+            return;
+        }
+
+        for (Entrenamiento entrenamiento : rutina.getEntrenamientos()) {
+            for (Ejercicio ejercicio : entrenamiento.getEjercicios()) {
+                // Incrementar repeticiones, series y peso a cada ejercicio
+                ejercicio.setRepeticiones(ejercicio.getRepeticiones() + 1);
+                ejercicio.setSeries(ejercicio.getSeries() + 1);
+                ejercicio.setPeso(ejercicio.getPeso() + 2.5);
+            }
+        }
     }
 }
